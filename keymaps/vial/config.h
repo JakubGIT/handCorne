cfc11a8d928a210f32ebbb62d043351db0d786a0
Jakub <jakub.kopecky@gmail.com>
2024-01-01 17:06:21 +0100

    Initiate repository

---
+++ keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x13, 0xD6, 0xDF, 0x41, 0x8E, 0xF2, 0x35, 0xB5}
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 4 }
